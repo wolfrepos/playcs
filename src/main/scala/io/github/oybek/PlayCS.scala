c9309b85393fa68e75a75a51ee1d09bfcf018a9c
oybek <aybek.hashimov@gmail.com>
2020-04-12 01:08:19 +0500

    write simple process interaction

--- src/main/scala/io/github/oybek/PlayCS.scala
+++ src/main/scala/io/github/oybek/PlayCS.scala
@@ -1,13 +1,29 @@
 package io.github.oybek
 
+import java.io.PrintWriter
+
 import cats.syntax.all._
 import cats.effect._
+
+import scala.io.Source
+import scala.sys.process.{Process, ProcessIO}
 
 object PlayCS extends IOApp {
 
   type F[+T] = IO[T]
 
-  def run(args: List[String]): IO[ExitCode] =
-    Sync[F].delay(println("Hello, world")).as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] = {
+    val process = Process("calc")
+    val octopus = new ProcessIO(
+      os => {
+        val pw = new PrintWriter(os)
+        pw.println("2+2")
+        pw.flush()
+      },
+      stdout => Source.fromInputStream(stdout).getLines.foreach(println),
+      stderr => Source.fromInputStream(stderr).getLines.foreach(println)
+    )
+    Sync[F].delay(process.run(octopus)).as(ExitCode.Success)
+  }
 
 }
