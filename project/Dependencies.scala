34e536f888985b3740da6488e2ef94dac50b7df8
oybek <aybek.hashimov@gmail.com>
2022-03-17 12:25:22 +0500

    update libraries

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,53 +3,32 @@ import sbt.*
 object Dependencies {
 
   object V {
-    val atto = "0.9.5"
-    val catsCore = "2.7.0"
-    val catsEffect = "3.3.3"
-    val scalaTest = "3.2.11"
-    val doobie = "1.0.0-RC1"
-    val testContainers = "0.39.12"
-    val flyway = "8.4.0"
-    val ciris = "2.3.1"
+    val atto           = "0.9.5"
+    val catsCore       = "2.7.0"
+    val catsEffect     = "3.3.7"
+    val ciris          = "2.3.2"
+    val doobie         = "1.0.0-RC2"
+    val flyway         = "8.5.4"
+    val scalaTest      = "3.2.11"
+    val telegramium    = "7.57.0"
+    val testContainers = "0.40.3"
   }
-
-  val catsCore   = "org.typelevel" %% "cats-core" % V.catsCore
-  val catsEffect = "org.typelevel" %% "cats-effect" % V.catsEffect
-  val scalaTest  = "org.scalatest" %% "scalatest" % V.scalaTest % Test
-  val flyway     = "org.flywaydb" % "flyway-core" % V.flyway
-  val ciris      = "is.cir" %% "ciris" % V.ciris
-
-  val doobie     = Seq(
-    "org.tpolecat" %% "doobie-core" % V.doobie,
-    "org.tpolecat" %% "doobie-hikari" % V.doobie,
-    "org.tpolecat" %% "doobie-postgres" % V.doobie
-  )
-
-  val testContainers = Seq(
-    "com.dimafeng" %% "testcontainers-scala-scalatest" % V.testContainers % Test,
-    "com.dimafeng" %% "testcontainers-scala-postgresql" % V.testContainers % Test
-  )
-
-  val atto = Seq(
-    "org.tpolecat" %% "atto-core"    % V.atto,
-    "org.tpolecat" %% "atto-refined" % V.atto
-  )
-
-  val telegramium = Seq(
-    "io.github.apimorphism" %% "telegramium-core" % "7.56.0",
-    "io.github.apimorphism" %% "telegramium-high" % "7.56.0"
-  )
 
   val common: Seq[ModuleID] =
     Seq(
-      catsCore,
-      catsEffect,
-      ciris,
-      flyway,
-      scalaTest,
-    ) ++
-    atto ++
-    telegramium ++
-    testContainers ++
-    doobie
+      "com.dimafeng"          %% "testcontainers-scala-postgresql" % V.testContainers % Test,
+      "com.dimafeng"          %% "testcontainers-scala-scalatest"  % V.testContainers % Test,
+      "io.github.apimorphism" %% "telegramium-core"                % V.telegramium,
+      "io.github.apimorphism" %% "telegramium-high"                % V.telegramium,
+      "is.cir"                %% "ciris"                           % V.ciris,
+      "org.flywaydb"          %  "flyway-core"                     % V.flyway,
+      "org.scalatest"         %% "scalatest"                       % V.scalaTest % Test,
+      "org.tpolecat"          %% "atto-core"                       % V.atto,
+      "org.tpolecat"          %% "atto-refined"                    % V.atto,
+      "org.tpolecat"          %% "doobie-core"                     % V.doobie,
+      "org.tpolecat"          %% "doobie-hikari"                   % V.doobie,
+      "org.tpolecat"          %% "doobie-postgres"                 % V.doobie,
+      "org.typelevel"         %% "cats-core"                       % V.catsCore,
+      "org.typelevel"         %% "cats-effect"                     % V.catsEffect,
+    )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "1.1.0")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.1")
